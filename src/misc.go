396cb60a206c01d561b15438f2bb33db9f88e7f1
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2021-06-15 20:54:43 +0200

    Remove unused function

--- src/misc.go
+++ src/misc.go
@@ -1,14 +1,5 @@
 package main
 
-func isExtensionPDF(filepath string) bool {
-	if len(filepath) > 4 {
-		if filepath[len(filepath)-4:] == ".pdf" {
-			return true
-		}
-	}
-	return false
-}
-
 func isExtensionHTML(filepath string) bool {
 	if len(filepath) > 5 {
 		if filepath[len(filepath)-5:] == ".html" {
